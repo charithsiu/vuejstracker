c994646621dcbbfb6dd1acc3aafcef791ca91695
Charith <owner@Chariths-MacBook-Pro.local>
2019-05-15 23:46:18 -0500

    add server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,16 @@
 module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es6": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/essential"
-    ],
+    "extends": "plugin:vue/essential",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        "ecmaVersion": 2018
     },
     "plugins": [
         "vue"
